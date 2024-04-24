966f8381657a705183ece65a9279a906601b8e77
Akinro Destined <akinro.destined@gmail.com>
2024-04-24 17:04:05 +0100

    worked on the help page

--- src/Pages/Support/Support.tsx
+++ src/Pages/Support/Support.tsx
@@ -1,9 +1,180 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../css/Support.css";
+import { FaSearch } from "react-icons/fa";
+import { Accordion } from "react-bootstrap";
 
 const Support = () => {
-	return (
-		<div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi ut aut mollitia tempore autem numquam impedit, reiciendis eos quo quidem ducimus deserunt. Odit tenetur explicabo beatae cumque? Vitae repudiandae accusamus cumque suscipit necessitatibus nihil alias corporis vero. Voluptatibus, sunt veniam corrupti, dicta consequatur iure, consectetur odio enim dignissimos beatae qui?</div>
-	)
-}
+  const [activeKey, setActiveKey] = useState<any>(null);
 
-export default Support
+  const handleAccordionClick = (eventKey: any) => {
+    if (activeKey === eventKey) {
+      setActiveKey(null);
+    } else {
+      setActiveKey(eventKey);
+    }
+  };
+  return (
+    <div className='support-page'>
+      <div className='support-page-top'>
+        <h1>Frequenty Asked Questions</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          explicabo fugiat aperiam non iste deleniti asperiores harum modi
+          perferendis! Veniam molestias quo temporibus consectetur qui.
+        </p>
+      </div>
+      <div className='support-page-search'>
+        <h3>Have any questions?</h3>
+        <div className='support-page-search-bar'>
+          <input type='search' />
+          <FaSearch size={27} className='icon' />
+        </div>
+      </div>
+      <div className='half-background'>
+        <Accordion activeKey={activeKey} onSelect={handleAccordionClick}>
+          <div>
+            <Accordion.Item eventKey='0'>
+              <div id='accordion-body-form'>
+                <Accordion.Header>Employee Details</Accordion.Header>
+                <Accordion.Body>
+                  <div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Aut quaerat animi exercitationem ab maiores, quasi labore!
+                      A nam magni quam cupiditate laboriosam sint explicabo unde
+                      animi asperiores facere optio officiis ratione est
+                      nesciunt sapiente expedita, nemo aperiam? Quisquam,
+                      voluptas! Veniam laboriosam dolorem libero officia fugit
+                      voluptatibus qui, natus quos! At ullam deserunt explicabo,
+                      amet vel quam cumque maiores. Voluptatibus odio saepe
+                      sapiente voluptates dolor at necessitatibus velit totam
+                      nostrum aliquam, minus quod repellendus vel dolore maiores
+                      temporibus error ipsa porro magni provident labore quae,
+                      beatae veritatis! Ab eos consequuntur aperiam quidem
+                      pariatur iure reprehenderit tempora labore quo nam
+                      suscipit recusandae architecto minus porro, repellat quis
+                      excepturi quod et iusto? Autem suscipit voluptatibus
+                      delectus est vel reprehenderit voluptatum doloremque sunt
+                      sapiente!
+                    </p>
+                  </div>
+                </Accordion.Body>
+              </div>
+            </Accordion.Item>
+            <Accordion.Item eventKey='1'>
+              <div id='accordion-body-form'>
+                <Accordion.Header>Address</Accordion.Header>
+                <Accordion.Body>
+                  <div>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Quidem praesentium ipsam nemo quaerat itaque vitae? Magni
+                      officiis saepe, possimus enim minus labore magnam sequi id
+                      voluptate natus eligendi culpa suscipit. Repellendus
+                      fugiat, dolorem, consequuntur quasi amet obcaecati
+                      distinctio non, eligendi ut consectetur nemo eveniet
+                      molestias! Voluptatum perspiciatis tempore, ducimus iure
+                      numquam, consequuntur minus similique dignissimos autem
+                      delectus laborum saepe dolorum quia labore qui soluta
+                      ratione necessitatibus a sint illo eos. Sunt, ipsam quas
+                      dolores dolor ea obcaecati tempore possimus accusamus sed
+                      officia iste blanditiis perspiciatis, quasi ratione velit
+                      adipisci nulla eligendi facilis rerum enim corporis
+                      aspernatur. Porro aut dolorum modi perspiciatis aliquid
+                      quas voluptas rerum veniam repellat molestias. Incidunt,
+                      exercitationem ipsum. Temporibus quidem corrupti mollitia
+                      dolores? Hic in reiciendis quaerat.
+                    </p>
+                  </div>
+                </Accordion.Body>
+              </div>
+            </Accordion.Item>
+            <Accordion.Item eventKey='2'>
+              <div id='accordion-body-form'>
+                <Accordion.Header>Employment Details</Accordion.Header>
+                <Accordion.Body>
+                  <div>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Quaerat minima itaque quo harum reiciendis rem unde eaque
+                      commodi iure consectetur. Id, quidem laudantium a velit
+                      dicta asperiores repellendus officia enim labore eos ut
+                      tempore praesentium quia deleniti dolores debitis quis quo
+                      ullam quam eum. Adipisci dignissimos alias aspernatur fuga
+                      amet dolore, magnam distinctio repellendus sunt dolores
+                      beatae iste ad animi dicta aut. Vero vitae natus est
+                      porro, nemo officia vel quidem non beatae laboriosam nulla
+                      nisi! Et deserunt id atque tenetur. Maiores perspiciatis
+                      neque saepe nobis nihil mollitia provident aperiam,
+                      deleniti tempore ad possimus dolores? Ratione, omnis
+                      maiores! Facere eaque vero qui. Ipsam laboriosam,
+                      molestiae aliquid, obcaecati tenetur odit delectus ducimus
+                      exercitationem aspernatur architecto cupiditate corrupti
+                      ratione optio voluptatem et.
+                    </p>
+                  </div>
+                </Accordion.Body>
+              </div>
+            </Accordion.Item>
+            <Accordion.Item eventKey='3'>
+              <div id='accordion-body-form'>
+                <Accordion.Header>Finance Details</Accordion.Header>
+                <Accordion.Body>
+                  <div>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Quaerat minima itaque quo harum reiciendis rem unde eaque
+                      commodi iure consectetur. Id, quidem laudantium a velit
+                      dicta asperiores repellendus officia enim labore eos ut
+                      tempore praesentium quia deleniti dolores debitis quis quo
+                      ullam quam eum. Adipisci dignissimos alias aspernatur fuga
+                      amet dolore, magnam distinctio repellendus sunt dolores
+                      beatae iste ad animi dicta aut. Vero vitae natus est
+                      porro, nemo officia vel quidem non beatae laboriosam nulla
+                      nisi! Et deserunt id atque tenetur. Maiores perspiciatis
+                      neque saepe nobis nihil mollitia provident aperiam,
+                      deleniti tempore ad possimus dolores? Ratione, omnis
+                      maiores! Facere eaque vero qui. Ipsam laboriosam,
+                      molestiae aliquid, obcaecati tenetur odit delectus ducimus
+                      exercitationem aspernatur architecto cupiditate corrupti
+                      ratione optio voluptatem et.
+                    </p>
+                  </div>
+                </Accordion.Body>
+              </div>
+            </Accordion.Item>
+            <Accordion.Item eventKey='4'>
+              <div id='accordion-body-form'>
+                <Accordion.Header>References</Accordion.Header>
+                <Accordion.Body>
+                  <div>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Quaerat minima itaque quo harum reiciendis rem unde eaque
+                      commodi iure consectetur. Id, quidem laudantium a velit
+                      dicta asperiores repellendus officia enim labore eos ut
+                      tempore praesentium quia deleniti dolores debitis quis quo
+                      ullam quam eum. Adipisci dignissimos alias aspernatur fuga
+                      amet dolore, magnam distinctio repellendus sunt dolores
+                      beatae iste ad animi dicta aut. Vero vitae natus est
+                      porro, nemo officia vel quidem non beatae laboriosam nulla
+                      nisi! Et deserunt id atque tenetur. Maiores perspiciatis
+                      neque saepe nobis nihil mollitia provident aperiam,
+                      deleniti tempore ad possimus dolores? Ratione, omnis
+                      maiores! Facere eaque vero qui. Ipsam laboriosam,
+                      molestiae aliquid, obcaecati tenetur odit delectus ducimus
+                      exercitationem aspernatur architecto cupiditate corrupti
+                      ratione optio voluptatem et.
+                    </p>
+                  </div>
+                </Accordion.Body>
+              </div>
+            </Accordion.Item>
+          </div>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
