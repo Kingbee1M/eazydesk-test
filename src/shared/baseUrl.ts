2cbd3f680efaa6fb6169d444f973e489148cb7af
user <chinedu.go@gmail.com>
2024-05-23 11:01:51 +0100

    new

--- src/shared/baseUrl.ts
+++ src/shared/baseUrl.ts
@@ -1,6 +1,6 @@
  
-     export const baseUrl: any = "http://localhost:5000";
-   //export const baseUrl: any = "https://105.112.129.222"; 
+     //export const baseUrl: any = "http://localhost:5000";
+   export const baseUrl: any = "https://105.112.129.222"; 
  
 
 export const buildDynamicURL = (
