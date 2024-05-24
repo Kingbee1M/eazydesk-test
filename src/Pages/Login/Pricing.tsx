5f4eb1541ce433c9d4c5912ec6ac36f5afb0514c
user <chinedu.go@gmail.com>
2024-05-24 14:47:59 +0100

    update

--- src/Pages/Login/Pricing.tsx
+++ src/Pages/Login/Pricing.tsx
@@ -17,56 +17,10 @@ const Pricing = () => {
 								<div className='settings_main_after_login'>
 									<div>
 										<div className='settings_main_after_sup'>
-											{/* <h3>Plans</h3>
-											<p>Lorem ipsum dolor sit amet consectetur</p> */}
+
 										</div>
-										{/* */}
 										<section className="pricing-plans">
-											{/* <div className="pricing-card basic">
-												<div className="heading">
-													<h4>BASIC</h4>
-													<p>for small websites or blogs</p>
-												</div>
-												<p className="price">
-													$2
-													<sub>/month</sub>
-												</p>
-												<ul className="features">
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>1 domain</strong> name
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>10 GB</strong> of disk space
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>100GB</strong>of bandwidth
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>1 MySQL</strong> database
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>5 email</strong> accounts
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>cPanel</strong> control panel
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>Free SSL</strong> certificate
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>24/7</strong> support
-													</li>
-												</ul>
-												<button className="cta-btn">SELECT</button>
-											</div> */}
+
 											<div className="pricing-card standard">
 												<div className="heading">
 													<h4>STANDARD</h4>
@@ -112,55 +66,7 @@ const Pricing = () => {
 												</ul>
 												<button className="cta-btn">SELECT</button>
 											</div>
-											{/* <div className="pricing-card premium">
-												<div className="heading">
-													<h4>PREMIUM</h4>
-													<p>for small businesses</p>
-												</div>
-												<p className="price">
-													$10
-													<sub>/month</sub>
-												</p>
-												<ul className="features">
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>Unlimited</strong> domain name
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>100 GB</strong> of disk space
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>1TB </strong>of bandwidth
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>Unlimited MySQL</strong> database
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>Unlimited email</strong> accounts
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>cPanel</strong> control panel
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>Free SSL</strong> certificate
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>24/7 priority</strong> support
-													</li>
-													<li>
-														<i className="fa-solid fa-check"></i>
-														<strong>Advanced</strong> security features
-													</li>
-												</ul>
-												<button className="cta-btn">SELECT</button>
-											</div> */}
+
 										</section>
 									</div>
 								</div>
