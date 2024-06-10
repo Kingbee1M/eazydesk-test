b81fad33abb0a48031e752989d655d40e8c67fca
user <chinedu.go@gmail.com>
2024-06-10 12:53:01 +0100

    update to url

--- src/shared/baseUrl.ts
+++ src/shared/baseUrl.ts
@@ -1,7 +1,7 @@
  
     //  export const baseUrl: any = "http://localhost:5000";
-  export const baseUrl: any = "https://crm.outcess.com"; 
-  // export const baseUrl: any = "https://105.112.129.222"; 
+  // export const baseUrl: any = "https://crm.outcess.com"; 
+   export const baseUrl: any = "https://105.112.129.222"; 
  
 
 export const buildDynamicURL = (
